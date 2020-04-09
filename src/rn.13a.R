e18e55b9146b466ff9af2649ca14ee9803292e6a
orionzhou <zhoupenggeni@gmail.com>
2020-04-09 08:22:05 -0500

    rn13a

--- src/rn.13a.R
+++ src/rn.13a.R
@@ -4,91 +4,85 @@ yid = 'rn13a'
 dirw = file.path(dird, '11_qc', yid)
 if(!dir.exists(dirw)) system(sprintf("mkdir -p %s", dirw))
 
-#{{{ read in, filter/fix samples
-ref = t_cfg %>% filter(yid == !!yid) %>% pull(ref)
-th = rnaseq_sample_meta(yid)
-tt = rnaseq_mapping_stat(yid)
+#{{{ read in
 res = rnaseq_cpm_raw(yid)
 th = res$th; tm = res$tm; tl = res$tl; th_m = res$th_m; tm_m = res$tm_m
-sum_stat_tibble(tt)
 
-sids_keep = tt %>% filter(mapped>5) %>% pull(SampleID)
-sum_stat_tibble(tt %>% filter(SampleID %in% sids_keep))
+th = res$th %>%
+    mutate(lab = str_c(Genotype, Replicate, sep='_')) %>%
+    mutate(clab = ifelse(Replicate==1, Genotype, ''))
+tm = res$tm %>% filter(SampleID %in% th$SampleID) %>%
+    mutate(value=asinh(CPM))
+#}}}
 
-# fix th
-th2 = th %>% filter(SampleID %in% sids_keep)
+#{{{ hclust & tSNE
+p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='pearson',var.col='Genotype',
+    pal.col='viridis_d', expand.x=.1)
+ggsave(file.path(dirw, '11.hclust.p.pdf'), p1, width=8, height=12)
+
+p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='spearman',var.col='Genotype',
+    pal.col='viridis_d', expand.x=.1)
+ggsave(file.path(dirw, '11.hclust.s.pdf'), p1, width=8, height=12)
+
+p2 = plot_pca(tm,th,pct.exp=.7, pca.center=T, pca.scale=F,
+    var.shape='', var.col='', var.lab='clab', var.ellipse='Genotype',
+    legend.pos='top.left', legend.dir='v', pal.col='')
+ggsave(file.path(dirw, '11.pca.pdf'), p2, width=8, height=8)
+
+p3 = plot_tsne(tm,th,pct.exp=.7,perp=10,iter=1500, seed=42,
+    var.shape='', var.col='', var.lab='clab', var.ellipse='Genotype',
+    legend.pos='top.right', legend.dir='v', pal.col='aaas')
+ggsave(file.path(dirw, '11.tsne.pdf'), width=8, height=8)
+#}}}
+
+#{{{ fix
+th2 = res$th
 th2 = complete_sample_list(th2)
 
-
-th = th2
-tt = tt %>% filter(SampleID %in% th$SampleID)
-
-fh = file.path(dirw, 'meta.tsv')
-write_tsv(th, fh, na='')
-# run snakemake again
+fh = file.path(dirw, '01.meta.tsv')
+write_tsv(th2, fh, na='')
 #}}}
 
+#{{{ read in again
 res = rnaseq_cpm(yid)
 th = res$th; tm = res$tm; tl = res$tl; th_m = res$th_m; tm_m = res$tm_m
-th = th %>% mutate(lab = sprintf("%s_%s", Genotype, Replicate))
 
-#{{{ hclust
-tw = tm %>% select(SampleID, gid, CPM) %>% mutate(CPM=asinh(CPM)) %>% spread(SampleID, CPM)
-t_exp = tm %>% group_by(gid) %>% summarise(n.exp = sum(CPM>=1))
-gids = t_exp %>% filter(n.exp >= (ncol(tw)-1) * .7) %>% pull(gid)
-e = tw %>% filter(gid %in% gids) %>% select(-gid)
-dim(e)
-
-cor_opt = "spearman"
-cor_opt = "pearson"
-hc_opt = "ward.D"
-hc_title = sprintf("dist: %s\nhclust: %s", cor_opt, hc_opt)
-edist <- as.dist(1-cor(e, method = cor_opt))
-ehc <- hclust(edist, method = hc_opt)
-tree = as.phylo(ehc)
-lnames = ehc$labels[ehc$order]
-#
-tp = th %>% mutate(taxa = SampleID) %>%
-    select(taxa, everything())
-p1 = ggtree(tree, layout = 'rectangular') +
-    scale_x_continuous(expand = expand_scale(0,.1)) +
-    scale_y_discrete(expand = c(.01,0))
-p1 = p1 %<+%
-    tp + geom_tiplab(aes(label=lab, color=Genotype), size=2.5) +
-    scale_color_viridis_d()
-fo = sprintf("%s/21.cpm.hclust.pdf", dirw)
-ggsave(p1, filename = fo, width=8, height=12)
+th = th %>%
+    mutate(lab = str_c(Genotype, Replicate, sep='_')) %>%
+    mutate(clab = ifelse(Replicate==1, Genotype, ''))
+tm = res$tm %>% filter(SampleID %in% th$SampleID) %>%
+    mutate(value=asinh(CPM))
 #}}}
 
-#{{{ tSNE
-require(Rtsne)
-tw = tm %>% select(SampleID, gid, CPM) %>% mutate(CPM=asinh(CPM)) %>% spread(SampleID, CPM)
-t_exp = tm %>% group_by(gid) %>% summarise(n.exp = sum(CPM>=1))
-gids = t_exp %>% filter(n.exp >= (ncol(tw)-1) * .7) %>% pull(gid)
-tt = tw %>% filter(gid %in% gids)
-dim(tt)
-tsne <- Rtsne(t(as.matrix(tt[-1])), dims=2, verbose=T, perplexity=10,
-              pca = T, max_iter = 1500)
+#{{{ hclust & tSNE
+p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='pearson',var.col='Genotype',
+    pal.col='viridis_d', expand.x=.1)
+ggsave(file.path(dirw, '21.hclust.p.pdf'), p1, width=8, height=12)
 
-tp = as_tibble(tsne$Y) %>%
-    add_column(SampleID = colnames(tt)[-1]) %>%
-    inner_join(th, by = 'SampleID')
-x.max=max(tp$V1)
-p_tsne = ggplot(tp) +
-    geom_text_repel(aes(x=V1,y=V2,label=Genotype), size=2.5) +
-    geom_point(aes(x=V1, y=V2, color=Genotype, shape=as.character(Replicate)), size=2) +
-    scale_x_continuous(name = 'tSNE-1') +
-    scale_y_continuous(name = 'tSNE-2') +
-    scale_shape_manual(values = c(15,0)) +
-    scale_color_viridis_d(name = 'Genotype',direction=-1) +
-    #scale_color_ucscgb(name = 'hours after cold') +
-    otheme(legend.pos='top.left', legend.dir='v', legend.title=T,
-           xtitle=T, ytitle=T,
-           margin = c(.2,.2,.2,.2)) +
-    theme(axis.ticks.length = unit(0, 'lines')) +
-    guides(color = F)
-fp = file.path(dirw, "25.tsne.pdf")
-ggsave(p_tsne, filename = fp, width=8, height=8)
+p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='spearman',var.col='Genotype',
+    pal.col='viridis_d', expand.x=.1)
+ggsave(file.path(dirw, '21.hclust.s.pdf'), p1, width=8, height=12)
+
+p2 = plot_pca(tm,th,pct.exp=.7, pca.center=T, pca.scale=F,
+    var.shape='', var.col='', var.lab='clab', var.ellipse='Genotype',
+    legend.pos='top.left', legend.dir='v', pal.col='')
+ggsave(file.path(dirw, '21.pca.pdf'), p2, width=8, height=8)
+
+p3 = plot_tsne(tm,th,pct.exp=.7,perp=10,iter=1500, seed=42,
+    var.shape='', var.col='', var.lab='clab', var.ellipse='Genotype',
+    legend.pos='top.right', legend.dir='v', pal.col='aaas')
+ggsave(file.path(dirw, '21.tsne.pdf'), width=8, height=8)
+#}}}
+
+gts = c("B73","Mo17")
+gts1 = c("M0021","M0317","M0014","M0016")
+gts2 = c('M0275','M0352','M0326','M0323','M0035','M0012','M0017','M0001')
+sids_red = th %>% filter(Genotype %in% c(gts, gts1, gts2)) %>% pull(SampleID)
+#{{{ RIL haplotype blocks
+cp = res$ril$cp
+p = plot_ril_genotype(cp, th, sids_red=sids_red)
+fo = file.path(dirw, '41.ril.genotype.pdf')
+ggsave(fo, p, width=8, height=15)
 #}}}
 
 
@@ -141,17 +135,6 @@ p1 = ggtree(tree) %<+% tp +
     guides(color=F)
 fo = file.path(dirw, '43.phylogeny.pdf')
 ggsave(p1, file=fo, width=6, height=15)
-#}}}
-
-gts = c("B73","Mo17")
-gts1 = c("M0021","M0317","M0014","M0016")
-gts2 = c('M0275','M0352','M0326','M0323','M0035','M0012','M0017','M0001')
-sids_red = th %>% filter(Genotype %in% c(gts, gts1, gts2)) %>% pull(SampleID)
-#{{{ RIL haplotype blocks
-cp = res$ril$cp
-p = plot_ril_genotype(cp, th, sids_red=sids_red)
-fo = file.path(dirw, '41.ril.genotype.pdf')
-ggsave(fo, p, width=8, height=15)
 #}}}
 
 
