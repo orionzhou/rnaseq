30cf8757209cbe35191c593250a7aaf29ca8aac8
orionzhou <zhoupenggeni@gmail.com>
2020-04-09 23:27:33 -0500

    updated rn20a

--- data/11_qc/rn20a/README.md
+++ data/11_qc/rn20a/README.md
@@ -1,1 +1,30 @@
-../template.ase.md
+## Raw samples:
+* [sample meta](https://github.com/orionzhou/barn/tree/master/data/15_read_list)
+* [hierarchical clustering w. pearson correlation](11.hclust.p.pdf)
+* [hierarchical clustering w. spearman correlation](11.hclust.s.pdf)
+* [PCA clustering](11.pca.pdf)
+* [tSNE clustering](11.tsne.pdf)
+
+## Cleaned samples:
+* [sample meta](01.meta.tsv)
+* Experiment 1: TC
+  * [hierarchical clustering w. pearson correlation](21.hclust.TC.p.pdf)
+  * [hierarchical clustering w. spearman correlation](21.hclust.TC.s.pdf)
+  * [tSNE clustering](21.tsne.TC.pdf)
+* Experiment 2: HY
+  * [hierarchical clustering w. pearson correlation](21.hclust.HY.p.pdf)
+  * [hierarchical clustering w. spearman correlation](21.hclust.HY.s.pdf)
+  * [tSNE clustering](21.tsne.HY.pdf)
+* Experiment 3: NM
+  * [hierarchical clustering w. pearson correlation](21.hclust.NM.p.pdf)
+  * [hierarchical clustering w. spearman correlation](21.hclust.NM.s.pdf)
+  * [tSNE clustering](21.tsne.NM.pdf)
+
+## ASE analysis
+* [SNP-level allele frequency distribution](32.afs_site.pdf)
+* [gene-level allele frequency distribution](31.afs_gene.pdf)
+
+File location on MSI:
+`/home/springer/zhoux379/projects/rnaseq/data/11_qc/rn20a/01.rds`
+
+[Result file walk through](https://github.com/orionzhou/rnaseq/blob/master/output.md)
--- src/rn.20a.R
+++ src/rn.20a.R
@@ -57,24 +57,105 @@ tm = res$tm %>% filter(SampleID %in% th$SampleID) %>%
     mutate(value=asinh(CPM))
 #}}}
 
-#{{{ hclust & tSNE
-p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='pearson',var.col='Experiment',
+#{{{ sample clustering
+#{{{ TC
+ex = 'TC'
+th1 = th %>% filter(Experiment==ex) %>%
+    mutate(Genotype = factor(Genotype, levels=gts3)) %>%
+    mutate(Treatment = factor(Treatment, levels=c("Control",'Cold','Heat'))) %>%
+    mutate(has = sprintf("h%03d", Timepoint*10)) %>%
+    mutate(grp = sprintf("%s_%s_%s", Treatment, has, Genotype)) %>%
+    mutate(cond = sprintf("%s_%s", Treatment, has)) %>%
+    arrange(Genotype,Treatment,Timepoint) %>%
+    group_by(Genotype, Treatment, Timepoint) %>%
+    mutate(lab = cond)
+tm1 = tm %>% filter(SampleID %in% th1$SampleID) %>%
+    mutate(value=asinh(CPM))
+
+p1 = plot_hclust(tm1,th1,pct.exp=.7,cor.opt='pearson',var.col='Genotype',
     expand.x=.2)
-ggsave(file.path(dirw, '21.hclust.p.pdf'), p1, width=8, height=24)
+ggsave(sprintf("%s/21.hclust.%s.p.pdf",dirw,ex), p1, width=6, height=8)
 
-p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='spearman',var.col='Experiment',
+p1 = plot_hclust(tm1,th1,pct.exp=.7,cor.opt='spearman',var.col='Genotype',
     expand.x=.2)
-ggsave(file.path(dirw, '21.hclust.s.pdf'), p1, width=8, height=24)
+ggsave(sprintf("%s/21.hclust.%s.s.pdf",dirw,ex), p1, width=6, height=8)
 
-p2 = plot_pca(tm,th,pct.exp=.7, pca.center=T, pca.scale=F,
-    var.shape='Treatment', var.col='Experiment', var.lab='Genotype',
-    legend.pos='top.center.out', legend.dir='h', legend.box='h', pal.col='aaas')
-ggsave(file.path(dirw, '21.pca.pdf'), p2, width=8, height=8)
+p2 = plot_tsne(tm1,th1,pct.exp=.5,perp=4,iter=900, seed=12,
+    var.shape='Treatment',var.col='Genotype',var.lab='has',#var.ellipse='grp',
+    legend.pos='top.center.out', legend.dir='h', legend.box='h',legend.title=F,
+    shapes=c(0,1,2), pal.col='aaas')
+ggsave(sprintf("%s/22.tsne.%s.pdf", dirw, ex), p2, width=6, height=6)
 
-p3 = plot_tsne(tm,th,pct.exp=.7,perp=8,iter=1500, seed=2,
-    var.shape='Treatment', var.col='Experiment', var.lab='Genotype',
-    legend.pos='top.center.out', legend.dir='h', legend.box='h', pal.col='aaas')
-ggsave(file.path(dirw, '21.tsne.pdf'), width=8, height=8)
+p3 = plot_umap(tm1,th1,pct.exp=.5,seed=42,
+    var.shape='Genotype',var.col='Genotype',var.lab='clab',var.ellipse='grp',
+    legend.pos='top.center.out', legend.dir='h', legend.box='h',legend.title=F,
+    pal.col='aaas')
+ggsave(sprintf("%s/22.umap.%s.pdf", dirw, ex), p3, width=6, height=6)
+#}}}
+
+#{{{ HY
+ex = 'HY'
+th1 = th %>% filter(Experiment==ex) %>%
+    mutate(Genotype = factor(Genotype, levels=gts6)) %>%
+    mutate(grp = sprintf("%s_%dh_%s", Treatment, Timepoint, Genotype)) %>%
+    mutate(cond = sprintf("%s_%dh", Treatment, Timepoint)) %>%
+    arrange(Genotype,Treatment,Timepoint) %>%
+    group_by(Genotype, Treatment, Timepoint) %>%
+    mutate(Replicate = 1:n()) %>% ungroup() %>%
+    mutate(lab = str_c(SampleID, grp, sep=' ')) %>%
+    mutate(clab = ifelse(Replicate==1, cond, ''))
+tm1 = tm %>% filter(SampleID %in% th1$SampleID) %>%
+    mutate(value=asinh(CPM))
+
+p1 = plot_hclust(tm1,th1,pct.exp=.7,cor.opt='pearson',var.col='Experiment',
+    expand.x=.2)
+ggsave(sprintf("%s/21.hclust.%s.p.pdf",dirw,ex), p1, width=8, height=10)
+
+p1 = plot_hclust(tm1,th1,pct.exp=.7,cor.opt='spearman',var.col='Experiment',
+    expand.x=.2)
+ggsave(sprintf("%s/21.hclust.%s.s.pdf",dirw,ex), p1, width=8, height=10)
+
+p2 = plot_tsne(tm1,th1,pct.exp=.5,perp=4,iter=900, seed=12,
+    var.shape='Genotype',var.col='Genotype',var.lab='clab',var.ellipse='grp',
+    legend.pos='top.center.out', legend.dir='h', legend.box='h',legend.title=F,
+    shapes=c(0:3,5,6), pal.col='aaas')
+ggsave(sprintf("%s/22.tsne.%s.pdf", dirw, ex), p2, width=6, height=6)
+
+p3 = plot_umap(tm1,th1,pct.exp=.5,seed=42,
+    var.shape='Genotype',var.col='Genotype',var.lab='clab',var.ellipse='grp',
+    legend.pos='top.center.out', legend.dir='h', legend.box='h',legend.title=F,
+    pal.col='aaas')
+ggsave(sprintf("%s/22.umap.%s.pdf", dirw, ex), p3, width=6, height=6)
+#}}}
+
+#{{{ NM
+ex = 'NM'
+th1 = th %>% filter(Experiment==ex) %>%
+    mutate(Genotype = factor(Genotype, levels=gts25)) %>%
+    mutate(Treatment = factor(Treatment, levels=c("Control",'Cold'))) %>%
+    mutate(grp = sprintf("%s_%dh_%s", Treatment, Timepoint, Genotype)) %>%
+    mutate(cond = sprintf("%s_%dh", Treatment, Timepoint)) %>%
+    mutate(cond = factor(cond, levels=c("Control_1h",'Control_25h','Cold_1h','Cold_25h'))) %>%
+    arrange(Genotype,Treatment,Timepoint) %>%
+    group_by(Genotype, Treatment, Timepoint) %>%
+    mutate(lab = str_c(cond, Genotype, sep="_"))
+tm1 = tm %>% filter(SampleID %in% th1$SampleID) %>%
+    mutate(value=asinh(CPM))
+
+p1 = plot_hclust(tm1,th1,pct.exp=.7,cor.opt='pearson',var.col='Treatment',
+    expand.x=.2)
+ggsave(sprintf("%s/21.hclust.%s.p.pdf",dirw,ex), p1, width=6, height=10)
+
+p1 = plot_hclust(tm1,th1,pct.exp=.7,cor.opt='spearman',var.col='Treatment',
+    expand.x=.2)
+ggsave(sprintf("%s/21.hclust.%s.s.pdf",dirw,ex), p1, width=6, height=10)
+
+p2 = plot_tsne(tm1,th1,pct.exp=.6,perp=4,iter=1200, seed=12,
+    var.shape='cond',var.col='cond',var.lab='Genotype',#var.ellipse='Genotype',
+    legend.pos='top.center.out', legend.dir='h', legend.box='h',legend.title=F,
+    shapes=c(0:1,15:16), pal.col='aaas')
+ggsave(sprintf("%s/22.tsne.%s.pdf", dirw, ex), p2, width=6, height=6)
+#}}}
 #}}}
 
 #{{{ ase
