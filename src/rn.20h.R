480f1aacf8fc5dfc7cecfdf2422f92ea4b119757
orionzhou <zhoupenggeni@gmail.com>
2020-04-13 12:43:29 -0500

    rn20h

--- src/rn.20h.R
+++ src/rn.20h.R
@@ -1,36 +1,36 @@
 source("functions.R")
 
-yid = 'rn'
+yid = 'rn20h'
 dirw = file.path(dird, '11_qc', yid)
-if(!dir.exists(dirw)) system(sprintf("mkdir -p %s", dirw))
+if(!dir.exists(dirw)) dir.create(dirw)
 
 #{{{ raw: read in
 res = rnaseq_cpm_raw(yid)
 th = res$th; tm = res$tm; tl = res$tl; th_m = res$th_m; tm_m = res$tm_m
 
 th = res$th %>%
-    mutate(lab = str_c(Genotype, Treatment, Replicate, sep='_'))
+    mutate(lab = str_c(SampleID, sep='_'))
 tm = res$tm %>% filter(SampleID %in% th$SampleID) %>%
     mutate(value=asinh(CPM))
 #}}}
 
 #{{{ raw: hclust & tSNE
-p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='pearson',var.col='Genotype',
-    expand.x=.25)
-ggsave(file.path(dirw, '11.hclust.p.pdf'), p1, width=6, height=8)
+p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='pearson',var.col='SampleID',
+    pal.col='viridis_d', expand.x=.25)
+ggsave(file.path(dirw, '11.hclust.p.pdf'), p1, width=4, height=5)
 
-p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='spearman',var.col='Genotype',
-    expand.x=.25)
-ggsave(file.path(dirw, '11.hclust.s.pdf'), p1, width=6, height=8)
+p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='pearson',var.col='SampleID',
+    pal.col='viridis_d', expand.x=.25)
+ggsave(file.path(dirw, '11.hclust.s.pdf'), p1, width=4, height=5)
 
 p2 = plot_pca(tm,th,pct.exp=.7, pca.center=T, pca.scale=F,
-    var.shape='Treatment', var.col='Genotype', var.lab='Replicate',
-    legend.pos='top.left', legend.dir='v', pal.col='aaas')
+    var.shape='', var.col='', var.lab='SampleID',
+    legend.pos='none', legend.dir='v', pal.col='aaas')
 ggsave(file.path(dirw, '11.pca.pdf'), p2, width=6, height=6)
 
 p3 = plot_tsne(tm,th,pct.exp=.7,perp=3,iter=1200, seed=2,
-    var.shape='Treatment', var.col='Genotype', var.lab='Replicate',
-    legend.pos='top.right', legend.dir='v', pal.col='aaas')
+    var.shape='', var.col='', var.lab='SampleID',
+    legend.pos='none', legend.dir='v', pal.col='aaas')
 ggsave(file.path(dirw, '11.tsne.pdf'), p3, width=6, height=6)
 #}}}
 
@@ -38,7 +38,7 @@ ggsave(file.path(dirw, '11.tsne.pdf'), p3, width=6, height=6)
 th2 = res$th
 th2 = complete_sample_list(th2)
 
-fh = file.path(dirw, 'meta.tsv')
+fh = file.path(dirw, '01.meta.tsv')
 write_tsv(th2, fh, na='')
 #}}}
 
