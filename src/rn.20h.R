8f5e0c1766eeb5836f5e7d20a2062d09466c3a7a
orionzhou <zhoupenggeni@gmail.com>
2020-04-13 16:09:01 -0500

    updated rn20h

--- src/rn.20h.R
+++ src/rn.20h.R
@@ -9,28 +9,29 @@ res = rnaseq_cpm_raw(yid)
 th = res$th; tm = res$tm; tl = res$tl; th_m = res$th_m; tm_m = res$tm_m
 
 th = res$th %>%
-    mutate(lab = str_c(SampleID, sep='_'))
+    mutate(lab = str_c(SampleID, Genotype, Treatment, Tissue, sep='_')) %>%
+    mutate(clab = str_c(Treatment, Tissue, sep='_'))
 tm = res$tm %>% filter(SampleID %in% th$SampleID) %>%
     mutate(value=asinh(CPM))
 #}}}
 
 #{{{ raw: hclust & tSNE
-p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='pearson',var.col='SampleID',
-    pal.col='viridis_d', expand.x=.25)
-ggsave(file.path(dirw, '11.hclust.p.pdf'), p1, width=4, height=5)
+p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='pearson',var.col='Genotype',
+    pal.col='aaas', expand.x=.5)
+ggsave(file.path(dirw, '11.hclust.p.pdf'), p1, width=5, height=5)
 
-p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='pearson',var.col='SampleID',
-    pal.col='viridis_d', expand.x=.25)
-ggsave(file.path(dirw, '11.hclust.s.pdf'), p1, width=4, height=5)
+p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='pearson',var.col='Genotype',
+    pal.col='aaas', expand.x=.5)
+ggsave(file.path(dirw, '11.hclust.s.pdf'), p1, width=5, height=5)
 
 p2 = plot_pca(tm,th,pct.exp=.7, pca.center=T, pca.scale=F,
-    var.shape='', var.col='', var.lab='SampleID',
-    legend.pos='none', legend.dir='v', pal.col='aaas')
+    var.shape='Genotype', var.col='Genotype', var.lab='clab',
+    legend.pos='top.left', legend.dir='v',legend.box='v', pal.col='aaas')
 ggsave(file.path(dirw, '11.pca.pdf'), p2, width=6, height=6)
 
-p3 = plot_tsne(tm,th,pct.exp=.7,perp=3,iter=1200, seed=2,
-    var.shape='', var.col='', var.lab='SampleID',
-    legend.pos='none', legend.dir='v', pal.col='aaas')
+p3 = plot_tsne(tm,th,pct.exp=.7,perp=4,iter=1200, seed=2,
+    var.shape='Genotype', var.col='Genotype', var.lab='clab',
+    legend.pos='top.left', legend.dir='v', pal.col='aaas')
 ggsave(file.path(dirw, '11.tsne.pdf'), p3, width=6, height=6)
 #}}}
 
@@ -47,10 +48,30 @@ res = rnaseq_cpm(yid)
 th = res$th; tm = res$tm; tl = res$tl; th_m = res$th_m; tm_m = res$tm_m
 
 th = res$th %>%
-    mutate(lab = str_c(Genotype, Treatment, Replicate, sep='_'))
+    mutate(lab = str_c(SampleID, Genotype, Treatment, Tissue, sep='_')) %>%
+    mutate(clab = str_c(Treatment, Tissue, sep='_'))
 tm = res$tm %>% filter(SampleID %in% th$SampleID) %>%
     mutate(value=asinh(CPM))
 #}}}
 
 
+#{{{ raw: hclust & tSNE
+p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='pearson',var.col='Genotype',
+    pal.col='aaas', expand.x=.5)
+ggsave(file.path(dirw, '21.hclust.p.pdf'), p1, width=5, height=5)
+
+p1 = plot_hclust(tm,th,pct.exp=.7,cor.opt='pearson',var.col='Genotype',
+    pal.col='aaas', expand.x=.5)
+ggsave(file.path(dirw, '21.hclust.s.pdf'), p1, width=5, height=5)
+
+p2 = plot_pca(tm,th,pct.exp=.7, pca.center=T, pca.scale=F,
+    var.shape='Genotype', var.col='Genotype', var.lab='clab',
+    legend.pos='top.left', legend.dir='v',legend.box='v', pal.col='aaas')
+ggsave(file.path(dirw, '21.pca.pdf'), p2, width=6, height=6)
+
+p3 = plot_tsne(tm,th,pct.exp=.7,perp=3,iter=1200, seed=2,
+    var.shape='Genotype', var.col='Genotype', var.lab='clab',
+    legend.pos='top.left', legend.dir='v', pal.col='aaas')
+ggsave(file.path(dirw, '21.tsne.pdf'), p3, width=6, height=6)
+#}}}
 
